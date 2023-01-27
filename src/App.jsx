e2a877e86aacfe2751a59790624f0b9df7817a2b
Mauro Colucci <colucci.mauro@gmail.com>
2023-01-27 01:22:39 -0300

    sidenav with img and after/before

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,18 @@ function App() {
     <div className="flex">
       <Sidenav />
       <div className="p-7">
-        <h1 className="text-2xl font-semibold">Home Page</h1>
+        <div className="card w-96 bg-base-100 shadow-xl image-full">
+          <figure>
+            <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn">Buy Now</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,7 +1,22 @@
 import { useState } from "react";
-import { BsArrowLeftShort, BsSearch, BsChevronDown } from "react-icons/bs";
-import { AiFillEnvironment, AiOutlineFileText } from "react-icons/ai";
+import {
+  BsArrowLeftShort,
+  BsSearch,
+  BsChevronDown,
+  BsReverseLayoutTextSidebarReverse,
+  BsFillImageFill,
+  BsPerson,
+} from "react-icons/bs";
+import {
+  AiFillEnvironment,
+  AiOutlineBarChart,
+  AiOutlineFileText,
+  AiOutlineLogout,
+  AiOutlineMail,
+  AiOutlineSetting,
+} from "react-icons/ai";
 import { RiDashboardFill } from "react-icons/ri";
+import Image from "../assets/lastCovid.jpg";
 
 const Sidenav = () => {
   const [open, setOpen] = useState(true);
@@ -9,9 +24,10 @@ const Sidenav = () => {
   const Menu = [
     { title: "Dashboard" },
     { title: "Pages", icon: <AiOutlineFileText /> },
-    { title: "Media", spacing: true },
+    { title: "Media", spacing: true, icon: <BsFillImageFill /> },
     {
       title: "Projects",
+      icon: <BsReverseLayoutTextSidebarReverse />,
       submenu: true,
       submenuItems: [
         { title: "Submenu 1" },
@@ -19,20 +35,23 @@ const Sidenav = () => {
         { title: "Submenu 3" },
       ],
     },
-    { title: "Analytics" },
-    { title: "Inbox" },
-    { title: "Profile", spacing: true },
-    { title: "Settings" },
-    { title: "Logout" },
+    { title: "Analytics", icon: <AiOutlineBarChart /> },
+    { title: "Inbox", icon: <AiOutlineMail /> },
+    { title: "Profile", spacing: true, icon: <BsPerson /> },
+    { title: "Settings", icon: <AiOutlineSetting /> },
+    { title: "Logout", icon: <AiOutlineLogout /> },
   ];
   return (
     <nav
-      className={`bg-slate-700 h-screen p-5 pt-8 ${
+      className={`h-screen p-5 pt-8 ${
         open ? "w-72" : "w-20"
-      } duration-300 relative`}
+      } duration-300 relative object-cover bg-center z-50 shadow-[5px_0px_18px_rgba(0,0,0,0.3)] before:absolute before:top-0 before:left-0 before:h-full before:w-full before:bg-[#11101d4d] after:absolute after:top-0 after:left-0 after:h-full after:w-full after:z-[-1] after:opacity-70 after:bg-gradient-to-b after:from-[#282828] after:to-[#11111166] after:bg-[length:150%_150%]`}
+      style={{
+        backgroundImage: `url(${Image})`,
+      }}
     >
       <BsArrowLeftShort
-        className={`bg-white text-slate-700 text-3xl rounded-full absolute -right-3 top-9 border border-slate-700 cursor-pointer ${
+        className={`bg-white text-slate-700 text-3xl rounded-full absolute -right-3 top-9 border border-slate-700 cursor-pointer duration-500 ${
           !open && "rotate-180"
         }`}
         onClick={() => setOpen(!open)}
@@ -54,7 +73,7 @@ const Sidenav = () => {
       <div
         className={`flex items-center rounded-md bg-slate-500 mt-6 py-2 ${
           open ? "px-4" : "px-2.5"
-        } `}
+        } relative`}
       >
         <BsSearch className={`text-white cursor-pointer ${open && "mr-2"}`} />
         <input
@@ -72,7 +91,7 @@ const Sidenav = () => {
               key={item.title}
               className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-slate-500 rounded-md ${
                 item.spacing ? "mt-9" : "mt-2"
-              }`}
+              } relative`}
             >
               <span className="text-2xl">
                 {item.icon || <RiDashboardFill />}
@@ -96,7 +115,7 @@ const Sidenav = () => {
                 {item.submenuItems.map((submenuItem) => (
                   <li
                     key={submenuItem.title}
-                    className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-slate-500 rounded-md"
+                    className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-slate-500 rounded-md relative"
                   >
                     {submenuItem.title}
                   </li>
